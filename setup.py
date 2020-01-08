cadadc1cf56e42f3c9af891b3872b6dd54536820
Vedad Hadžić <vedad.hadzic@student.tugraz.at>
2020-01-08 11:52:15 +0100

    removed maple* solvers because they don't compile

--- setup.py
+++ setup.py
@@ -67,8 +67,9 @@ Details can be found at `https://pysathq.github.io <https://pysathq.github.io>`_
 
 # solvers to install
 #==============================================================================
-to_install = ['cadical', 'glucose30', 'glucose41', 'lingeling', 'maplechrono',
-        'maplecm', 'maplesat', 'minicard', 'minisat22', 'minisatgh']
+to_install = ['cadical', 'glucose30', 'glucose41', 'lingeling', 
+              # 'maplechrono', 'maplecm', 'maplesat', 
+              'minicard', 'minisat22', 'minisatgh']
 
 
 # example scripts to install as standalone executables
